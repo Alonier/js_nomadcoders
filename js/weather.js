74caa14b04dccccffb1c7a35cd676d02085474dc
Alonier_mac <junhee2727@gmail.com>
2024-05-17 17:02:30 +0900

    Nomadcoder Javascript Done

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,17 @@
+import {WEATHER_API_KEY} from "../values.js";
 
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log(lat,lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${WEATHER_API_KEY}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>{
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        weather.innerText = `${data.weather[0].main} ${data.main.temp}`;
+        city.innerText = data.name;
+    });
 }
 
 function onGeoError() {
